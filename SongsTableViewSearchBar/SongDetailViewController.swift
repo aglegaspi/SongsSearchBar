183c2bec5c504709588bb3e6b38c8a6ab60a0c0e
aglegaspi <lex83nyc@gmail.com>
2019-08-15 16:04:09 -0400

    created a new View Controller named SongDetailViewController

---
+++ SongsTableViewSearchBar/SongDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SongDetailViewController.swift
+//  SongsTableViewSearchBar
+//
+//  Created by Alexander George Legaspi on 8/15/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class SongDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
