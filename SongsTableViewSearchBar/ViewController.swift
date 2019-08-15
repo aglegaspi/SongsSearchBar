9dc25c94666719f8ea1fd2a5de4fb8bac05f8bf3
aglegaspi <lex83nyc@gmail.com>
2019-08-15 15:33:09 -0400

    added protocols for View Controller

--- SongsTableViewSearchBar/ViewController.swift
+++ SongsTableViewSearchBar/ViewController.swift
@@ -19,3 +19,21 @@ class ViewController: UIViewController {
 
 }
 
+// TABLE VIEW PROTOCOLS
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
+
+// SEARCH BAR PROTOCOLS
+extension ViewController: UISearchBarDelegate {
+    
+}
+
